7ea16ee326a3744bb56ac74270f6a19601ac0e92
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2025-03-29 18:19:48 -0400

    Add rudimentary gallery layout and initial styling

--- functions.js
+++ functions.js
@@ -1,25 +1,48 @@
 function fetchPhotos()
 {
-    // get the dive where the images should go
-    var $tn_div = $("#thumbs");
-    // just in case there's anything still in the thumbnails div, clear it out
-    $tn_div.empty();
+    // select the 3 cols where imgs will be populated
+    var $tn_col1 = $("#col1");
+    var $tn_col2 = $("#col2");
+    var $tn_col3 = $("#col3");
+    // just in case there's anything still in the thumbnail divs, clear them out
+    $tn_col1.empty();
+    $tn_col2.empty();
+    $tn_col3.empty();
 
     // retrieve images from the database
-    // !!! NB: change grp_id to match your group !!! //
     $endpoint = $path_to_backend + 'getPhotos.php?grp_id=' + $grp_id;
     $.getJSON($endpoint, function(data)
+    // divide images into 3 to separate into each column
+    // layout by column instead of row to keep varying image heights
+
     {
+        var col_length = ((data.length) / 3);
         $.each(data, function(key, val)
         {
-            // append the images to the div, and make them clickable for details
-            $("<img />")
-                .attr("src", $path_to_backend + val.tn_src)
+            if(key < col_length){
+                // append the images to the div, and make them clickable for details
+                // using src instead of tn_src for the higher-quality images
+                $("<img />")
+                .attr("src", $path_to_backend + val.src)
                 .attr("id", val.id)
-                .attr("class", "tn")
-                .appendTo($tn_div)
-                .wrap('<a href="' + $path_to_backend + val.tn_src + '"></a>')
-                .onClick(alert("clicked"));
+                .attr("class", "img-fluid thumbnail")
+                .appendTo($tn_col1)
+                .wrap('<a href="' + $path_to_backend + val.src + '"></a>')
+            }else if((key >= col_length) && key < (col_length*2)){
+                $("<img />")
+                .attr("src", $path_to_backend + val.src)
+                .attr("id", val.id)
+                .attr("class", "thumbnail img-fluid")
+                .appendTo($tn_col2)
+                .wrap('<a href="' + $path_to_backend + val.src + '"></a>')
+            }else{
+                $("<img />")
+                .attr("src", $path_to_backend + val.src)
+                .attr("id", val.id)
+                .attr("class", "thumbnail img-fluid")
+                .appendTo($tn_col3)
+                .wrap('<a href="' + $path_to_backend + val.src + '"></a>')
+            }
         });
 
         // make our thumbnails draggable
