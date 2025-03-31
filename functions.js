a5f440f88d16b5365c38b809a231ae32737c88c6
Jordan Schrier <121615335+jordanschrier@users.noreply.github.com>
2025-03-31 01:08:34 -0400

    New function to display full image with its description

--- functions.js
+++ functions.js
@@ -20,33 +20,36 @@ function fetchPhotos()
         $.each(data, function(key, val)
         {
             if(key < col_length){
-                // append the images to the div, and make them clickable for details
-                // using src instead of tn_src for the higher-quality images
+                // append the images to their respective columns and trigger uploadFunction when clicked
+                // using src instead of tn_src for the higher-quality images. longer loading times though
                 $("<img />")
                 .attr("src", $path_to_backend + val.src)
                 .attr("id", val.id)
                 .attr("class", "img-fluid thumbnail")
                 .appendTo($tn_col1)
-                .wrap('<a href="' + $path_to_backend + val.src + '"></a>')
+                .click(function() {
+                    clickPhoto(val.id);
+                });
             }else if((key >= col_length) && key < (col_length*2)){
                 $("<img />")
                 .attr("src", $path_to_backend + val.src)
                 .attr("id", val.id)
                 .attr("class", "thumbnail img-fluid")
                 .appendTo($tn_col2)
-                .wrap('<a href="' + $path_to_backend + val.src + '"></a>')
+                .click(function() {
+                    clickPhoto(val.id);
+                });
             }else{
                 $("<img />")
                 .attr("src", $path_to_backend + val.src)
                 .attr("id", val.id)
                 .attr("class", "thumbnail img-fluid")
                 .appendTo($tn_col3)
-                .wrap('<a href="' + $path_to_backend + val.src + '"></a>')
+                .click(function() {
+                    clickPhoto(val.id);
+                });
             }
         });
-
-        // make our thumbnails draggable
-        
     });
 };
 
@@ -87,6 +90,44 @@ function uploadFunction()
     .done(function()
     {
         alert("photo uploaded");
+    });
+}
+
+/**
+ * Triggers when user clicks an image to open the 'vertical view.' 
+ * Currently just loads the clicked image. Must reload the page to view full gallery.
+ * In the future, will ensure the clicked image is loaded first, followed by the rest 
+ * of the images.
+ * @param {*} id 
+ */
+function clickPhoto(id) {
+    var $col = $("#column_section");
+    $col.empty();
+
+    //calling viewPhoto to get the single photos and their descriptions
+    $endpoint = $path_to_backend + 'viewPhoto.php?grp_id=' + $grp_id + '&id=' + id;
+    console.log($endpoint);
+    $.getJSON($endpoint, function(data){
+
+        //this re-renders the left column with the filters. will need to be adjusted once in place
+        $("<div>")
+            .attr("class", "filter-col")
+            .appendTo($col)
+
+        //renders full-sized image
+        $("<img />")
+            .attr("src", $path_to_backend + data[0].src)
+            .attr("class", "img-fluid full-image")
+            .appendTo($col)
+            .wrap('<div class="col-md px-2"></div>')
+
+        //renders white description box next to image
+        $("<div>")
+            .append("<h2>Description</h2>")
+            .append(data[0].description)
+            .appendTo($col)
+            .wrap('<div class="col-md px-2"></div>')
+            .attr("class", "description p-3")
     });
 }
 
